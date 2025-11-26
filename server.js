69f8b20892cc74a00955f07bce1dbce526c49ff7
focus-apps-2025 <focusenggapps@gmail.com>
2025-11-26 19:44:40 +0530

    Update catch-all route for React Router in Express

--- server.js
+++ server.js
@@ -91,11 +91,12 @@ if (process.env.NODE_ENV === 'production') {
   const clientBuildPath = path.join(__dirname, '../frontend/dist');
   app.use(express.static(clientBuildPath));
 
-  // catch-all for React Router
-  app.get('/*', (_, res) => {
+  // Express 5: use RegExp instead of "*"
+  app.get(/.*/, (_, res) => {
     res.sendFile(path.join(clientBuildPath, 'index.html'));
   });
 }
+
 
 
 /* ---------------------------------------------------------------------- */
